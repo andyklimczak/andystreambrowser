52eb21893e427acfe8a24ceae84e2cc64159efb3
andyklicmzak <andyklimczak@gmail.com>
2014-05-22 18:55:25 -0400

    redid streams_controller to make it load faster. works somewhat faster now

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -1,42 +1,26 @@
 class StreamsController < ApplicationController
-	require 'will_paginate/array' #required in order for will_paginate to paginate an array instead of an active record
+  require 'will_paginate/array'
   def index
-    number_of_streams = 48
-    @stream_list ||= []
-    @user_game_filter ||= []
-    #store a bunch of streams, to speed up the page and lower the number of total calls
-    @streams = Twitch.streams
-    #if the user is not signed in, show all games
+    number_of_streams = 64
+    @stream_list = []
+
+    #get unfiltered streams for non logged in user
     unless user_signed_in?
-      #make a list of unique stream names that are sorted by viewer count, ignoring game
-      @streams.all(limit:number_of_streams).each do |stream|
-        @stream_list.push(stream.channel.name)
+      Twitch.streams.all(limit:number_of_streams) do |stream|
+        @stream_list.push([stream.preview_url, stream.url])
       end
-    #if the user is signed in, only show the games that they have a preference for
+    #use User preferences and get a filtered list of streams
     else
-      user = User.find(current_user)
-      @user_game_filter = user.games
-      #get the user's game preferences
-      @user_game_filter = @user_game_filter.collect{|x| x.strip.chomp}
-
-      #add streams that are not filtered by the user's preference until the size of the stream_list equals number_of_streams
-      @streams.all(limit:number_of_streams*5).each do |stream|
-        unless @user_game_filter.include?(stream.game_name)
-          @stream_list.push(stream.channel.name)
+      #get the user's preferences
+      user_filter = User.find(current_user).games.collect{|x| x.strip.chomp}
+      #go through top streams, adding streams to the list only if they aren't filtered by the user
+      Twitch.streams.all(limit:number_of_streams*5) do |stream|
+        unless user_filter.include?(stream.game_name)
+          @stream_list.push([stream.preview_url, stream.url])
         end
-        break if @stream_list.length >= number_of_streams #this ensures that the same number of streams will be shown when logged in and logged out
+        break if @stream_list.length >= number_of_streams #limit the total number of streams to the list
       end
     end
-
-    #now that we have the stream names, get the stream preview and url
-    #this is used in the view
-    @stream_preview_and_url_list ||= []
-    @stream_list.each do |stream_name|
-      s = @streams.get(stream_name)
-      @stream_preview_and_url_list.push([s.preview_url, s.channel.url])
-    end
-
-  #paginate the list for the view
-	@stream_preview_and_url_list = @stream_preview_and_url_list.paginate(page: params[:page], per_page:16)
+    @stream_list = @stream_list.paginate(page:params[:page], per_page:16)
   end
 end
